23944d9fb785afc6630cc9c1fd1618b001934ac0
Demali-876 <90882773+Demali-876@users.noreply.github.com>
2024-02-18 18:50:15 -0500

    update

---
+++ src/portfolio_frontend/src/routes/contact.svelte
@@ -0,0 +1,16 @@
+<!-- src/routes/contact.svelte -->
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<div class="container">
+	<p>Contact Me on OpenChat</p>
+</div>
+
+
+<style>
+	 .container {
+        max-width: 800px;
+        margin: 50px auto;
+    }
+</style>
---
+++ src/portfolio_frontend/src/routes/projects.svelte
@@ -0,0 +1,16 @@
+<!-- src/routes/projects.svelte -->
+<svelte:head>
+	<title>Projects</title>
+</svelte:head>
+
+<div class="container">
+	<p>Soon To be implemented</p>
+</div>
+
+
+<style>
+	 .container {
+        max-width: 800px;
+        margin: 50px auto;
+    }
+</style>
