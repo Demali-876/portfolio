75536e01842ed0dc818fb126ebc9d0e29a995da3
Demali-876 <90882773+Demali-876@users.noreply.github.com>
2024-02-21 11:56:25 -0500

    persisted canister id

--- src/portfolio_frontend/src/routes/+layout.svelte
+++ src/portfolio_frontend/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <!-- src/routes/+layout.svelte -->
 <nav>
     <div class="nav-container">
-        <a href="/" class="nav-logo" title="Back to Homepage">Home</a>
+        <a href="/?canisterId=bd3sg-teaaa-aaaaa-qaaba-cai" class="nav-logo" title="Back to Homepage">Home</a>
         <div class="nav-links">
-            <a href="/about" class="link">About</a>
-            <a href="/projects" class="link">Projects</a>
-            <a href="/contact" class="link">Contact</a>
+            <a href="/about?canisterId=bd3sg-teaaa-aaaaa-qaaba-cai" class="link">About</a>
+            <a href="/projects?canisterId=bd3sg-teaaa-aaaaa-qaaba-cai" class="link">Projects</a>
+            <a href="/contact?canisterId=bd3sg-teaaa-aaaaa-qaaba-cai" class="link">Contact</a>
         </div>
     </div>
 </nav>
